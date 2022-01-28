142e366a29562d91288aa198dc10a15fccc81265
Folajimmie <hayodele24@gmail.com>
2022-01-28 07:11:34 +0100

    Folder work on section 4 of the landing page

--- src/components/Section4/Section4.jsx
+++ src/components/Section4/Section4.jsx
@@ -7,12 +7,39 @@ const Section4 = () => {
         <section>
             <div>
                 <div className="container_shell">    
-                    <h1 className="section_head">Our Lastest Project</h1> 
+                    <h1 className="section_head">Our Lastest <span> Projects</span></h1> 
                     <div className="section_card">
-                        <img src={CardImage} alt="image4" className="section_img"/>
-                        <div className="section_title">
-                        <h1>Construction of Zaramangada Road</h1>
-                        <p className="section_title">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum odio elementum at vel interdum.</p>
+                        <div className="section_card1">
+                            <img src={CardImage} alt="image4" className="section_img"/>
+                            <div className="local_govt">
+                                <p> JOS SOUTH</p>
+                            </div>
+                            <div className="section_title">
+                                <h1>Construction of Zaramangada Road</h1>
+                                <p className="section_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum odio elementum at vel interdum.</p>
+                            </div>   
+                        </div>
+                        
+                        <div className="section_card1">
+                            <img src={CardImage} alt="image4" className="section_img"/>
+                            <div className="local_govt">
+                                <p> JOS NORTH</p>
+                            </div>
+                            <div className="section_title">
+                                <h1>Renovation of Plateau State Specialist Hospital</h1>
+                                <p className="section_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum odio elementum at vel interdum.</p>
+                            </div>   
+                        </div>
+                        
+                        <div className="section_card1">
+                            <img src={CardImage} alt="image4" className="section_img"/>
+                            <div className="local_govt">
+                                <p> JOS SOUTH</p>
+                            </div>
+                            <div className="section_title">
+                                <h1>Construction of Jos Main Airport</h1>
+                                <p className="section_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Interdum odio elementum at vel interdum.</p>
+                            </div>   
                         </div>
                     </div>
                     
