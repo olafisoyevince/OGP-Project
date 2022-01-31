f7042a4f71fe863871755f71254da437b9f89cba
Samson Mccarthy <sammccarthy090@gmail.com>
2022-01-31 14:52:22 +0000

    Worked on the folder structure of Section 3

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
-import Section3 from "./Section3";
+import React from 'react';
+//import Navbar from '../Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import Section1 from '../Section1/Section-1';
+import path from "../routes.js";
+import Section3 from './Section3/Section3';
+
 function App() {
   return (
-    <div>
-      <Section3 />
-    </div>
-  );
-}
+    <BrowserRouter>
 
-export default App;
+      <Section3 />
+
+      <Routes>
+        <Route exact path={path.home} />
+        <Route exact path={path.reports} />
+        <Route exact path={path.about} />
+        <Route exact path={path.dashboard} />
+      </Routes>
+
+    </BrowserRouter>
+  );
+  }
+
+  export default App;
--- src/components/Section3/Section3.js
+++ src/components/Section3/Section3.js
@@ -1,84 +1,79 @@
 import React from "react";
-import Card from './Card.css';
+import './Card.css';
+import Wed from '../assets/images/[GetPaidStock 2.svg';
 
 
 const Section3 = () => {
-    // const cardInfo = [
-    //     {Image:'', title:'',text:''},
-    //     {Image:'', title:'',text:''},
-    //     {Image:'', title:'',text:''},
-    //     {Image:'', title:'',text:''},
-    //     {Image:'', title:'',text:''},
-    // ];
-    // const renderCard = (card, index) => {
-    //     <Card style = {{width:'18rem' }}></Card>
-        
-
-    return(
-     <div>
+    return (
+    <>
+     <div  style={{width:'100vw',display:'flex'}}>
+     <div className='daddy' style={{width:'100%', height:'100vh'}}>
         <section>
-            <div className="card-container">
-                <div className="card">
+            <div className="card-container"  >
+                <div className="card" style={{backgroundColor:'rgba(0, 59, 73, 0.75) ',}}>
             <div style={ { paddingTop:'12rem' }}> 
                    <img src="../images/bank-icon.svg" alt="Avatar" style={{width:"50px",}}/>
                    </div>
-                    <div className="text" style={{backgroundColor:'blue', marginTop:'0rem'}}>
-                    <h2>Report of Accountant<b></b> General</h2>
-                        <h4><b></b></h4>
+                    <div className="text" style={{ marginTop:'0rem'}}>
+                    <h2>Report of Accountant<br></br> General</h2>
+                        <h4>lorem<b></b>ipsum</h4>
                         <p></p>
                     </div> 
+                    
                 </div>
 
-                <div className="card">
+                <div className="card" style={{backgroundColor:'rgba(255, 125, 69, 0.75)' }} >
                     <div style={ { paddingTop:'12rem' }}> 
                     <img src="../images/cal-icon.svg" alt="Avatar" style={{width:"50px" }}/>
                     </div>
                     <div class="text">
+                    <h2>Quarterly <br></br>Report</h2>
                         <h4><b></b></h4>
                         <p>card</p>
                     </div> 
                 </div>
 
-                <div className="card">
+                <div className="card" style={{backgroundColor:'rgba(255, 255, 255, 0.60)'}}>
                 <div style={ { paddingTop:'12rem' }}> 
                     <img src="../images/document-icon.svg" alt="Avatar" style={{width:"50px"}}/>
                 </div>
                     <div class="text">
+                    <h2>Audit <br></br>Report</h2>
                         <h4><b></b></h4>
                         <p>card</p>
                     </div> 
                 </div>
-
+                  
                 <div className="card">
                 <div style={ { paddingTop:'12rem' }}> 
                     <img src="../images/download-icon.svg" alt="Avatar" style={{width:"50px"}}/>
                 </div>
                     <div class="text">
+                    <h2>All <br></br>Downloads</h2>
                         <h4><b></b></h4>
                         <p>card</p>
                     </div> 
                 </div>
             </div>
+      </section>
+      <section >
+        <div className='new-container' style={{backgroundColor:'', width:'100%'}}>
+            <div className="new-card" style={ { paddingTop:'18rem', backgroundColor:'#003B49 ' }}></div>
+            <img src={Wed} alt="Avatar" />
+        </div>
+     
+
 
       </section>
 
+      </div>
     
     
-    
-    </div> 
+    </div > 
+    </>
     )
 };
 
 
+
 export default Section3;
-
-
-
-
-/*
-Step1
--create 5 image tags
--use d pic as a background image 
-
-
-*/
