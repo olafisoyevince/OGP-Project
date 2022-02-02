7b556cf88299527e9c1cb2c034d596cb67e092f0
Seun Olafisoye <86838623+olafisoyevince@users.noreply.github.com>
2022-02-02 15:43:38 +0100

    Added App.js into App folder

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 //import Navbar from '../Navbar/Navbar';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 //import Section1 from '../Section1/Section-1';
-import path from "../routes.js";
-import Section3 from './Section3/Section3';
+import path from "../../routes.js";
+import Section3 from '../Section3/Section3';
 
 function App() {
   return (
